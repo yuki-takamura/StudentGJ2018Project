261b8938457d8e2263fc9613ea38a639951d1b25
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2018-07-08 06:55:01 +0900

    ぼたんをおしてねを消えるようにした

--- StudentGJ2018Project/Assets/Pyon/Script/FontFade.cs
+++ StudentGJ2018Project/Assets/Pyon/Script/FontFade.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FontFade : MonoBehaviour {
-
+public class FontFade : MonoBehaviour
+{
+    bool isChangingScene;
     Color color;
 
 	// Use this for initialization
 	void Start ()
     {
         color = Color.white;
+        isChangingScene = false;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        color.a = Mathf.Abs(Mathf.Sin(Time.time));
-        GetComponent<Text>().color = color;
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1)
+            || Input.GetKeyDown(KeyCode.Joystick2Button1)
+            || Input.GetKeyDown(KeyCode.Z))
+        {
+            isChangingScene = true;
+        }
+
+        if (isChangingScene)
+        {
+            color.a = 0;
+            GetComponent<Text>().color = color;
+        }
+        else
+        {
+            color.a = Mathf.Abs(Mathf.Sin(Time.time));
+            GetComponent<Text>().color = color;
+        }
 	}
 }
