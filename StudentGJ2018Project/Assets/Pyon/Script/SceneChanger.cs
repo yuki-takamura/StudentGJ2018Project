ab9226323a93641c86b4080e91b2b2a08cfdf86b
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2018-07-07 23:45:28 +0900

    シーンチェンジャーの汎用性を高めた　いんすぺくたからシーン名指定する

--- StudentGJ2018Project/Assets/Pyon/Script/SceneChanger.cs
+++ StudentGJ2018Project/Assets/Pyon/Script/SceneChanger.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour {
 
+    [SerializeField]
+    string nextSceneName;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -13,10 +16,12 @@ public class SceneChanger : MonoBehaviour {
 	void Update ()
     {
         //-の↓ボタン
-        if (Input.GetKey(KeyCode.Joystick1Button1))
+        if (Input.GetKey(KeyCode.Joystick1Button1)
+            || Input.GetKey(KeyCode.Joystick2Button1)
+            || Input.GetKey(KeyCode.Z))
         {
             Debug.Log("osareta");
-            SceneManager.LoadScene("Main");
+            SceneManager.LoadScene(nextSceneName);
         }
 	}
 }
