e454e0e2a07874ad1388bb94dcaa61d81c1b35a9
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2018-07-07 16:32:24 +0900

    簡単にシーン遷移を実装

---
+++ StudentGJ2018Project/Assets/Pyon/Script/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //-の↓ボタン
+        if (Input.GetKey(KeyCode.Joystick1Button1))
+        {
+            Debug.Log("osareta");
+            SceneManager.LoadScene("Main");
+        }
+	}
+}
