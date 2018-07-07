5aab5835b899b82db880ad54951d879fa6842283
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2018-07-08 00:55:55 +0900

    背景のみのフェードインをじっそう

---
+++ StudentGJ2018Project/Assets/Pyon/Script/SC_CustomPostEffectBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SC_CustomPostEffectBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    public Material postEffectMaterial = null;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, postEffectMaterial);
+    }
+}
