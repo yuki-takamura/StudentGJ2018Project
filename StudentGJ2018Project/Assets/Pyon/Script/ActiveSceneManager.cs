367cbb761381671c2c4904d8663111a2e985ce3c
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2018-07-08 10:43:17 +0900

    プレイヤーエントリーシーンが一応できちあ

---
+++ StudentGJ2018Project/Assets/Pyon/Script/ActiveSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActiveSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    Text text1 = null;
+
+    [SerializeField]
+    Text text2 = null;
+    
+    [SerializeField]
+    GameObject child;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(text1.GetComponent<PlayerEntry>().canPlayGame
+            && text2.GetComponent<PlayerEntry>().canPlayGame)
+        {
+            child.SetActive(true);
+        }
+    }
+}
---
+++ StudentGJ2018Project/Assets/Pyon/Script/PlayerEntry.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerEntry : MonoBehaviour
+{
+    [SerializeField]
+    bool isPlayerOne = true;
+
+    public bool canPlayGame = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        canPlayGame = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(isPlayerOne)
+        {
+            //Left
+            if(Input.GetKeyDown(KeyCode.Joystick2Button0)
+                || Input.GetKeyDown(KeyCode.Joystick2Button1)
+                || Input.GetKeyDown(KeyCode.Joystick2Button2)
+                || Input.GetKeyDown(KeyCode.Joystick2Button3))
+            {
+                canPlayGame = false;
+            }
+        }
+        else
+        {
+            //Right
+            if (Input.GetKeyDown(KeyCode.Joystick1Button0)
+                || Input.GetKeyDown(KeyCode.Joystick1Button1)
+                || Input.GetKeyDown(KeyCode.Joystick1Button2)
+                || Input.GetKeyDown(KeyCode.Joystick1Button3))
+            {
+                canPlayGame = false;
+            }
+        }
+
+        if(canPlayGame)
+        {
+            GetComponent<Text>().text = "準備OK!!!!";
+        }
+        else
+        {
+            GetComponent<Text>().text = "ボタンをおしてね";
+        }
+	}
+}
